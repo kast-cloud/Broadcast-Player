1527c17f59b442484a6ef698599fa808284da8ee
supriya <katalsupriya36@gmail.com>
2021-11-17 10:33:11 +0530

    KC-261

--- src/components/podcast_info.vue
+++ src/components/podcast_info.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="kc-podcast-info" v-if="podcastInfo">
+  <div class="kc-podcast-info"
+    v-if="podcastInfo && (podcastInfo.podcastDirectories || podcastInfo.rssUrl)">
     <div class="kc-podcast-info__cell--artwork">
       <img v-bind:src="podcastInfo.artworkUrlSml" alt="" class="kc-podcast-info__img">
     </div>
@@ -8,19 +9,21 @@
       <div class="kc-podcast-info__author">By {{ podcastInfo.author }}</div>
     </div>
     <div class="kc-podcast-info__cell--dir">
-      <a v-for="podcastDirectorie in podcastInfo.podcastDirectories" :key="podcastDirectorie.order"
-        v-bind:href="podcastDirectorie.url" class="kc-podcast-info__btn--img"
-        v-bind:title="`Listen on ${podcastDirectorie.name}`">
-        <img v-bind:src="podcastDirectorie.imageUrl"
-          v-bind:alt="`Listen on ${podcastDirectorie.name}`">
-        <span class="show-for-sr">Listen on {{podcastDirectorie.name}}</span>
+
+      <a v-for="podcastDirectorie in podcastInfo.podcastDirectories" target="_blank"
+        :key="podcastDirectorie.order" v-bind:href="podcastDirectorie.url"
+        class="kc-podcast-info__btn--img" v-bind:title="podcastDirectorie.artworkAltText">
+        <img v-bind:src="podcastDirectorie.imageUrl" v-bind:alt="podcastDirectorie.artworkAltText">
+        <span class="show-for-sr">{{podcastDirectorie.artworkAltText}}</span>
       </a>
+
       <a v-if="podcastInfo.rssUrl" v-bind:href="podcastInfo.rssUrl"
         class="kc-podcast-info__btn kc-podcast-info__btn--rss"
         title="Click to view the RSS feed URL">
         <span class="ki-rss"></span>
         RSS
       </a>
+
     </div>
   </div>
 </template>
