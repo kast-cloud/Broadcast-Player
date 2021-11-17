84a76e02914760f101b87e2497429bf465f96b29
supriya <katalsupriya36@gmail.com>
2021-11-17 11:58:17 +0530

    KC-262

--- src/components/episode_latest_list.vue
+++ src/components/episode_latest_list.vue
@@ -6,47 +6,9 @@
       </div>
       <div class="kc-wdg__sec--bdy">
         <ul class="kc-episode-list">
-          <li v-for="episode in latestEpisodes.items"><a href="#" class="kc-episode-sum">
-              <article class="kc-episode-sum__inner">
-                <div class="kc-episode-sum__img-wrp">
-                  <img v-bind:src="episode.artworkUrlSml" v-bind:alt="episode.title"
-                    class="kc-episode-sum__img">
-                </div>
-                <div class="kc-episode-sum__txt">
-                  <h1 class="kc-episode-sum__title">{{episode.title}}</h1>
-                  <dl class="ko-keyvalue ko-keyvalue--value-only kc-episode-sum__info">
-                    <div class="kc-episode-sum__released">
-                      <dt>Recorded</dt>
-                      <dd>
-                        <time class="ko-datetime">
-                          {{episode.releasedUtc | moment("ddd Do MMMM")}} <span
-                            class="kl-dt-separator">{{episode.releasedUtc | moment("h:mm a")}}</span></time>
-                      </dd>
-                    </div>
-                    <div class="kc-episode-sum__author">
-                      <dt>Author</dt>
-                      <dd>{{episode.author}}</dd>
-                    </div>
-                    <div class="kc-episode-sum__series" v-if="episode.series">
-                      <dt>Series</dt>
-                      <dd>{{episode.series.title}}</dd>
-                    </div>
-                    <div class="kc-episode-sum__location" v-if="episode.location">
-                      <dt>Location</dt>
-                      <dd>
-                        <span class="ko-badge ko-badge--location">{{episode.location.title}}</span>
-                      </dd>
-                    </div>
-                    <div class="kc-episode-sum__duration" v-if="episode.audio">
-                      <dt>Duration</dt>
-                      <dd>
-                        <i class="ki-clock"></i> {{episode.audio.duration}} min
-                      </dd>
-                    </div>
-                  </dl>
-                </div>
-              </article>
-            </a></li>
+            <EpisodeLatestListItem v-for="episode in latestEpisodes.items" v-bind:episode="episode">
+            </EpisodeLatestListItem>
+          </li>
         </ul>
       </div>
       <div class="kc-wdg__sec--ftr">
@@ -58,6 +20,8 @@
 
 <script>
   import Vue from 'vue';
+  import EpisodeLatestListItem from '@/components/episode_latest_list_item.vue';
+
   Vue.use(require('vue-moment'));
   export default {
     name: 'EpisodeLatestList',
@@ -75,6 +39,9 @@
     beforeMount() {
       this.getLatestEpisodes()
     },
+    components: {
+      EpisodeLatestListItem,
+    }
   };
 
 </script>
---
+++ src/components/episode_latest_list_item.vue
@@ -0,0 +1,55 @@
+<template>
+  <li>
+    <a href="#" class="kc-episode-sum">
+      <article class="kc-episode-sum__inner">
+        <div v-if="episode.artworkUrlSml" class="kc-episode-sum__img-wrp">
+          <img v-bind:src="episode.artworkUrlSml" v-bind:alt="episode.title"
+            class="kc-episode-sum__img">
+        </div>
+        <div class="kc-episode-sum__txt">
+          <h1 class="kc-episode-sum__title">{{episode.title}}</h1>
+          <dl class="ko-keyvalue ko-keyvalue--value-only kc-episode-sum__info">
+            <div class="kc-episode-sum__released">
+              <dt>Recorded</dt>
+              <dd>
+                <time class="ko-datetime">
+                  {{episode.releasedUtc | moment("ddd Do MMMM")}} <span
+                    class="kl-dt-separator">{{episode.releasedUtc | moment("h:mm a")}}</span></time>
+              </dd>
+            </div>
+            <div class="kc-episode-sum__author">
+              <dt>Author</dt>
+              <dd>{{episode.author}}</dd>
+            </div>
+            <div class="kc-episode-sum__series" v-if="episode.series">
+              <dt>Series</dt>
+              <dd>{{episode.series.title}}</dd>
+            </div>
+            <div class="kc-episode-sum__location" v-if="episode.location">
+              <dt>Location</dt>
+              <dd>
+                <span class="ko-badge ko-badge--location">{{episode.location.title}}</span>
+              </dd>
+            </div>
+            <div class="kc-episode-sum__duration" v-if="episode.audio">
+              <dt>Duration</dt>
+              <dd>
+                <i class="ki-clock"></i> {{episode.audio.duration}} min
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </article>
+    </a>
+  </li>
+</template>
+
+<script>
+  import Vue from 'vue';
+
+  export default {
+    name: 'EpisodeLatestListItem',
+    props: ['episode'],
+  };
+
+</script>
